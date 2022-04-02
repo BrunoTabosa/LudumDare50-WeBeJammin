2ca2dd162c6f72fb3bd233179600385bfc330c09
Bruno <brunotabosasnt@gmail.com>
2022-04-02 10:16:33 -0300

    - Added Carinho Slider
    TODO: Improve movement return for cat after Carinho

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -1,14 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Character : MonoBehaviour
 {
     public bool IsMovementActive = false;
     private Vector2 target;
     public float movementSpeed;
+    public Slider CarinhoSlider;
+
+    private float MaxCarinhoValue = 3f;
+    private float currentCarinhoValue = 0f;
 
     private GameManager gameManager;
+    
 
     public void Setup(GameManager gm)
     {
@@ -25,9 +31,44 @@ public class Character : MonoBehaviour
 
     private void Update()
     {
+        CarinhoSlider.value = currentCarinhoValue / MaxCarinhoValue;
 
+        
         if (!IsMovementActive) return;
         Move();
+    }
+
+    private void OnMouseDown()
+    {
+        IsMovementActive = false;
+        
+        
+    }
+
+    private void OnMouseDrag()
+    {
+        currentCarinhoValue += Time.deltaTime;
+
+        if(currentCarinhoValue >= MaxCarinhoValue)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        StopCoroutine(returnMovement(0));
+
+        
+        StartCoroutine(returnMovement(2f));
+    }
+
+
+
+    IEnumerator returnMovement(float seconds)
+    {
+        yield return new WaitForSecondsRealtime(seconds);
+        IsMovementActive = true;
     }
 
     public void Move()
