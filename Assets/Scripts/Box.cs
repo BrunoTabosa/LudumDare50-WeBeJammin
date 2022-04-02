99cd9c637579ba2c7776107d58238ec011c7cbc7
Bruno <brunotabosasnt@gmail.com>
2022-04-02 14:42:09 -0300

    Adding cat in the box

--- Assets/Scripts/Box.cs
+++ Assets/Scripts/Box.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Box : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject CanInTheBox;
+
+    private BoxManager boxManager;
+    private GameManager gameManager;
+    
+    void Setup(BoxManager bm, GameManager gm)
     {
-        
+        CanInTheBox.SetActive(false);
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if(collision.tag == "Character")
+        {
+            //Cat entering in the box
+
+            CanInTheBox.SetActive(true);
+            Destroy(collision.gameObject);
+            
+            // "Tell managers about that" -Karen
+        }
     }
+
+    
 }
